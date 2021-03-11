c525a8d1f4ef720f70d1d4c35fa3169724cec3c8
TheodoraE <tidoraa@gmail.com>
2021-03-11 10:44:42 +0100

    start

---
+++ database/migrations/2021_03_11_090505_create_players_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->integer('age');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('country_origin');
+
+            $table->unsignedBigInteger('gender_id');
+            $table->foreign('gender_id')->references('id')->on('genders');
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->unsignedBigInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->unsignedBigInteger('picture_id');
+            $table->foreign('picture_id')->references('id')->on('pictures');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
