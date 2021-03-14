3b566638c3b41c3eee58228f7ae3986e1f724c1c
Mustafa <startmk2020@gmail.com>
2021-03-15 00:17:06 +0100

    derniere rectification , desgin form ajout players

--- resources/views/pages/create/createPlayers.blade.php
+++ resources/views/pages/create/createPlayers.blade.php
@@ -1,8 +1,8 @@
 @extends('template.main')
 
 @section('content')
-    <div class="container">
-        <h1 style="text-decoration: underline" class="my-4">Add new players: </h1>
+    <div class="container bg-dark">
+        <h1 style="text-decoration: underline" class="my-4 text-warning">Add new players: </h1>
 
         <div>
             @if ($errors->any())
@@ -20,34 +20,34 @@
             @csrf
             <div class="row">
                 <div class="form-group col-6">
-                    <label for="lastname">Name : </label>
+                    <label  class="text-warning" for="lastname">Name : </label>
                     <input class="form-control" type="text" id="lastname" name="lastname" value="{{old('lastname')}}">
                 </div>
                 <div class="form-group col-6">
-                    <label for="firstname">Firstname : </label>
+                    <label  class="text-warning" for="firstname">Firstname : </label>
                     <input class="form-control" type="text" id="firstname" name="firstname" value="{{old('firstname')}}">
                 </div>
             </div>
             <div class="form-group">
-                <label for="age">Age : </label>
+                <label  class="text-warning" for="age">Age : </label>
                 <input class="form-control" type="number" id="age" name="age" value="{{old('age')}}">
             </div>
             <div class="form-group">
-                <label for="phone">Phone : </label>
+                <label  class="text-warning" for="phone">Phone : </label>
                 <input class="form-control" type="text" id="phone" name="phone" value="{{old('phone')}}">
             </div>
             <div class="form-group">
-                <label for="email">Email : </label>
+                <label  class="text-warning" for="email">Email : </label>
                 <input class="form-control" type="email" id="email" name="email" value="{{old('email')}}">
             </div>
             <div class="form-group">
-                <label for="country_origin">Origin Country : </label>
+                <label  class="text-warning" for="country_origin">Origin Country : </label>
                 <input class="form-control" type="text" id="country_origin" name="country_origin" value="{{old('country_origin')}}">
             </div>
 
 
             <div class="form-group">
-                <label for="gender_id">Gender :</label>
+                <label  class="text-warning" for="gender_id">Gender :</label>
                 <select class="form-control" id="gender_id" name="gender_id" value="">
                     @foreach ($genders as $gender)
                         <option value="{{$gender->id}}">{{$gender->gender}}</option>
@@ -55,7 +55,7 @@
                 </select>
             </div>
             <div class="form-group">
-                <label for="position_id">Position :</label>
+                <label  class="text-warning" for="position_id">Position :</label>
                 <select class="form-control" id="position_id" name="position_id" value="">
                     @foreach ($positions as $position)
                         <option value="{{$position->id}}">{{$position->position}}</option>
@@ -63,7 +63,7 @@
                 </select>
             </div>
             <div class="form-group">
-                <label for="team_id">Team :</label>
+                <label  class="text-warning" for="team_id">Team :</label>
                 <select class="form-control" id="team_id" name="team_id" value="">
                     <option value="">Sans equipe</option>
                     @foreach ($teams as $team)
@@ -74,8 +74,8 @@
 
 
             <div class="form-group">
-                <label for="url">Picture : </label>
-                <input type="file" id="url" name="url" value="silouhaite.jpeg">
+                <label  class="text-warning" for="url">Picture : </label>
+                <input  class="text-warning" type="file" id="url" name="url" value="silouhaite.jpeg">
             </div>
 
 
